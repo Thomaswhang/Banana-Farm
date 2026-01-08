41da21849864d9e903e4c1512d019f2225a539d7
SuriyoVerweij <302376335@student.rocmondriaan.nl>
2026-01-08 15:16:04 +0100

    Banana Farm Start. Opdracht 1 en 2 Af

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+console.log("Opdracht Banana Farm: Start")
+
+const bananaFarm = document.querySelectorAll(".banana-tree")
+const statusDisplay = document.querySelector("#status-display")
+const resetBtn = document.querySelector("#reset-button")
+
+let gameState = {
+    bananasToPick: 5,
+    bananasPicked: 0,
+    gameIsActive: true
+};
+
+function updateDisplay() {
+    bruh
+}
+
+updateDisplay()
